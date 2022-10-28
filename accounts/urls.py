d073f9e7ac9c722dcdfb9984c30dfc72641d3f60
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-10-28 10:30:54 +0600

    Initial
    Создана модель Пользователя, формы регистрации и входа, соответствующие вьюшки (+ изменение данных пользоваеля), пути, шаблоны и миграции.

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/change/', UserChangeView.as_view(), name='change')
+]
